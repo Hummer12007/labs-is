2f8a2071b598917006d294d93e352c9c28f63bad
Костя Луценко <lutsenkos2016@gmail.com>
2023-05-03 21:35:21 +0300

    lab3

---
+++ visual.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <SFML/Graphics.hpp>
+
+struct point
+{
+    double x, y;
+};
+
+void Display(int n, point* p, std::vector<int> order, sf::RenderWindow* window, int wsize);
